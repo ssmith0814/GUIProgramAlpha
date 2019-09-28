cd8b43bb6abe1595123c4844c8b283fcba1f323b
Shane Smith <shanesmith98@gmail.com>
2019-09-28 18:05:28 -0400

    End of Sprint 1

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,45 @@
 package sample;
+//imports
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
 
 public class Controller {
+
+    private ObservableList<String> chooseQuantity = FXCollections.observableArrayList("1", "2", "3", "4", "5", "6", "7", "8", "9", "10");
+
+    @FXML // fx:id="AddProductBtn"
+    private Button AddProductBtn; // Value injected by FXMLLoader
+
+    @FXML // fx:id="RecordProductionsBtn"
+    private Button RecordProductionsBtn; // Value injected by FXMLLoader
+
+    @FXML // fx:id="QuantityComboBox"
+    private ComboBox<String> QuantityComboBox; // Value injected by FXMLLoader
+
+    //Giving the add production button an output
+    @FXML
+    void AddProductBtnPressed(ActionEvent event) {
+        System.out.println("INSERT INTO Product(type, manufacturer, name)");
+    }
+    //Giving the record productions button an output
+    @FXML
+    void RecordProductionsBtnPressed(ActionEvent event) {
+        System.out.println("Production Recorded");
+    }
+
+    @FXML
+    public void initialize() {
+        //Giving the combo box options
+        QuantityComboBox.setItems(chooseQuantity);
+        //Making the combo box editable
+        QuantityComboBox.setEditable(true);
+        //making the default selection be the first option
+        QuantityComboBox.getSelectionModel().selectFirst();
+    }
+
 }
+
+
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,21 +1,46 @@
 package sample;
-
+//imports
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+/**
+ * @author Shane Smith
+ */
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    //setting the default look of the page
+    public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Production Tracker");
+        primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
     }
-
+    //Connects to database
+    public static void ping(String sql) {
+        System.out.println(sql);
+        final String JDBC_DRIVER = "org.h2.Driver";
+        final String DB_URL = "jdbc:h2:./res/PTI";
+        final String USER = "";
+        final String PASS = "";
+        try {
+            Class.forName(JDBC_DRIVER);
+            Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+            Statement stmt = conn.createStatement();
+            stmt.executeUpdate(sql);
+            stmt.close();
+            conn.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
 
     public static void main(String[] args) {
         launch(args);
