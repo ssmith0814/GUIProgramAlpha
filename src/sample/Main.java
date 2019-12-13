7b1957ff965d89c0f9197421ceba7527b09bdd2f
Shane Smith <shanesmith98@gmail.com>
2019-12-12 23:32:18 -0500

    added some css

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -20,6 +20,8 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
+  private static Statement stnt;
+
   @Override
   //setting the default look of the page
   public void start(Stage primaryStage) throws Exception {
@@ -27,24 +29,31 @@ public class Main extends Application {
     primaryStage.setTitle("Production Tracker");
     primaryStage.setScene(new Scene(root, 600, 400));
     primaryStage.show();
+    initializeDB();
+  }
+
+  public static void sqlExecute(String sqlStatment) {
+    try {
+      stnt.executeUpdate(sqlStatment);
+      System.out.println(sqlStatment);
+    } catch (Exception ex) {
+      ex.printStackTrace();
+    }
   }
 
   /***************************************************
    * Connects to database.
    * @author Shane Smith
-   * @param sql This is the sql string that gets access to the database.
    */
-  public static void initializeDB(String sql) {
-    System.out.println(sql);
+  public static void initializeDB() {
     final String jdbcDriver = "org.h2.Driver";
-    final String dbUrl = "jdbc:h2:./res/PTI";
+    final String dbUrl = "jdbc:h2:./res/databas";
     final String user = "";
     final String pass = "";
     try {
       Class.forName(jdbcDriver);
       Connection conn = DriverManager.getConnection(dbUrl, user, pass);
       Statement stmt = conn.createStatement();
-      stmt.executeUpdate(sql);
       stmt.close();
       conn.close();
     } catch (Exception ex) {
